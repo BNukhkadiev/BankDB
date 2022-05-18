8a411e1e532ce02227bc1f5c0ecd6f7e9600c1ce
NukhkadievBS <magnushasrp@gmail.com>
2022-05-18 17:20:07 +0300

    Added Template Inheritance. Also Added Accounts page

--- app.py
+++ app.py
@@ -25,18 +25,8 @@ class Account(db.Model):
     """
     id = db.Column(db.Integer(), primary_key=True)
     balance = db.Column(db.Float(), nullable=False)
-    client_id = db.ForeignKey(Client.id)
+    client_id = db.Column(db.Integer, db.ForeignKey(Client.id))
     currency = db.String(db.String(length=15))
-
-
-class Credit(db.Model):
-    """
-    Класс таблицы содержащей данные об оформленных кредитах
-    """
-    id = db.Column(db.Integer(), primary_key=True)
-    percentage = db.Column(db.Float(), nullable=False)
-    credit_amount = db.Column(db.Float, nullable=False)
-    last_payment_date = db.Column(db.Date, nullable=False)
 
 
 class Transfer(db.Model):
@@ -44,8 +34,8 @@ class Transfer(db.Model):
     Класс таблицы содержащей данные о переводах
     """
     id = db.Column(db.Integer(), primary_key=True)
-    receiver_account_id = db.ForeignKey(Account.id)
-    sender_account_id = db.ForeignKey(Account.id)
+    receiver_account_id = db.Column(db.Integer, db.ForeignKey(Account.id))
+    sender_account_id = db.Column(db.Integer, db.ForeignKey(Account.id))
     transfer_amount = db.Column(db.Float, nullable=False)
 
 
@@ -59,31 +49,43 @@ class Employee(db.Model):
 
 
 # Добавить эти таблицы (У них не работает Foreign KEy посмотри как это пофиксить в репозитории heelo flask
-# class Credit(db.Model):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     employee_id = db.ForeignKey(Employee.id)
-#     client_id = db.ForeignKey(Client.id)
-#     percentage = db.Column(db.Float(), nullable=False)
-#     credit_amount = db.Column(db.Float(), nullable=False)
-#     last_payment_date = db.Column(db.Date(), nullable=False)
-#
-#
-# class Service(db.Model):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     service_name = db.Column(db.String(225), nullable=False)
-#
-#
-# class Payment(db.Model):
-#     id = db.Column(db.Integer(), primary_key=True)
-#     payment_amount = db.Column(db.Float(), nullable=False)
-#     employee_id = db.ForeignKey(Employee.id)
-#     account_id = db.ForeignKey(Account.id)
+class Credit(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey(Employee.id))
+    client_id = db.Column(db.Integer, db.ForeignKey(Client.id))
+    percentage = db.Column(db.Float(), nullable=False)
+    credit_amount = db.Column(db.Float(), nullable=False)
+    last_payment_date = db.Column(db.Date(), nullable=False)
+
+
+class Service(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    service_name = db.Column(db.String(225), nullable=False)
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    payment_amount = db.Column(db.Float(), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey(Employee.id))
+    account_id = db.Column(db.Integer, db.ForeignKey(Account.id))
 
 
 @app.route('/')
-def index():
+@app.route('/home')
+def home_page():
     name = request.args.get('name', 'world')
     return render_template('index.html', name=name)
+
+
+@app.route("/accounts")
+def accounts_page():
+    accounts = [
+        {'id': 1, 'name': 'Pavel', 'balance': 100},
+        {'id': 2, 'name': 'Ivan', 'balance': 200},
+        {'id': 3, 'name': 'Brian', 'balance': 5523},
+        {'id': 4, 'name': 'Kornelius', 'balance': 3123}
+    ]
+    return render_template('accounts.html', accounts=accounts)
 
 
 # @app.route("/register", methods=["POST"])
