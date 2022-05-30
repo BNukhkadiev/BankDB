c5e5cf169c65f3f3bed1d69f7519b68bf5385bf2
NukhkadievBS <magnushasrp@gmail.com>
2022-05-31 02:24:14 +0300

    Done with console_interface.py

--- console_interface/console_interface.py
+++ console_interface/console_interface.py
@@ -1,103 +1,113 @@
-from sqlalchemy import create_engine
-from sqlalchemy import text, inspect
-from sqlalchemy import MetaData
-from sqlalchemy import select
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import registry
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import result
+import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Column, Integer, Float, ForeignKey, String, Date, Table
+from sqlalchemy import select, text, inspect
+from prettytable import PrettyTable, from_db_cursor
 
-from prettytable import PrettyTable
-
-from bank.models import Client, Account, Employee
-
-engine = create_engine("sqlite:///bank/bank.db", future=True)
-metadata = MetaData()
-Session = sessionmaker(bind=engine, future=True)
-session = Session()
+# establish connections
+engine = create_engine("sqlite:///../bank/bank.db")
+inspector = inspect(engine)
 
 
-def select_all(table_name):
+# initialize the Metadata Object
+# meta = MetaData(bind=engine)
+
+
+# MetaData.reflect(meta)
+
+
+# create a table schema
+class Account:
+    __table__ = "account"
+    id = Column(Integer, primary_key=True)
+    balance = Column(Float, nullable=False)
+    client_id = Column(Integer, ForeignKey("client.id"))
+    email = Column(String)
+    password = Column(String)
+
+
+class Credit:
+    __table__ = "credit"
+    id = Column(Integer, primary_key=True)
+    employee_id = Column(Integer, ForeignKey("employee.id"))
+    client_id = Column(Integer, ForeignKey("client.id"))
+    percentage = Column(Float)
+    credit_amount = Column(Float)
+    last_payment_date = Column(Date)
+
+
+class Employee:
+    __table__ = "employee"
+
+
+class Transfer:
+    __table__ = "transfer"
+
+
+class Client:
+    __table__ = "client"
+
+
+def add_values(table_name, *values):
+    values = tuple(values)
+    query = f"INSERT INTO {table_name} VALUES {values}"
+    with engine.begin() as connection:
+        connection.execute(text(query))
+    print(values)
+
+
+def select_table(table_name):
+    query = f"SELECT * FROM {table_name}"
+    with engine.begin() as connection:
+        rows = connection.execute(text(query)).all()
     x = PrettyTable()
-    with engine.connect() as connection:
-        rows = connection.execute(text("select * from employee")).all()
-    x.field_names = list(rows._mapping.keys())
+    x.field_names = rows[0]._mapping.keys()
     x.add_rows(rows)
     print(x)
 
 
-# def add():
-#     with engine.connect() as connection:
-#         connection.execute(
-#             text("insert into employee (emp_name) values (:emp_name)"),
-#             {"emp_name": "mr. krabs"}
-#         )
-#         connection.commit()
+def delete_from_table(table_name, condition):
+    query = f"DELETE FROM {table_name} WHERE {condition}"
+    with engine.begin() as connection:
+        connection.execute(text(query))
 
 
-input("Choose an option:\n"
-      "1. Add values\n"
-      "2. Print table\n"
-      "3. Delete values\n"
-      "4. Select rows\n")
+def search_values(table_name, condition):
+    query = f"SELECT * FROM {table_name} WHERE {condition}"
+    with engine.begin() as connection:
+        rows = connection.execute(text(query)).all()
+    x = PrettyTable()
+    x.field_names = rows[0]._mapping.keys()
+    x.add_rows(rows)
+    print(x)
 
 
-def print_tables():
-    inspector = inspect(engine)
-    for i in inspector.get_table_names():
-        print(i)
+def print_table_names():
+    print("Available tables:", "; ".join(inspector.get_table_names()))
 
 
-# print("Available tables: ")
-# print_tables()
+while True:
+    option = input("Choose an option:\n"
+                   "1. Add new values\n"
+                   "2. Delete values\n"
+                   "3. Print table\n"
+                   "4. Search by condition\n"
+                   "5. Print table names\n")
+    if len(option) == 0:
+        break
 
-# print(select())
+    table_name = input("Table name: ")
 
-
-# print(User.__table__)
-# print(select(User.__table__))
-#
-# spongebob = User(username="spongebob", fullname="Spongebob Squarepants")
-# print(spongebob)
-
-# with engine.begin() as connection:
-#     mapper_registry.metadata.create_all(connection)
-#
-# session.add(spongebob)
-# print(session.new)
-
-# select_statement = select(User).filter_by(username="spongebob")
-# result = session.execute(select_statement)
-# print(result)
-#
-# also_spongebob = result.scalar()
-# print(also_spongebob)
-
-
-# session.add_all([
-#     Employee(name="spongebob", position="cook"),
-#     Employee(name="squidward", position="manager")
-# ])
-#
-# session.commit()
-
-# result = session.execute(
-#     select(User).where(User.username.in_(["spongebob", "fakeuser"]))
-# )
-#
-# print(result.all())
-#
-
-# print(User.username == "spongebob")
-#
-def print_table():
-    query = (
-        select(Account)
-    )
-
-    result = session.execute(query)
-    print(result)
-    for row in result.scalars():
-        print(row)
-
-print_table()
-
+    if option == "1":
+        values = input("Values: ").split()
+        add_values(table_name, *values)
+    if option == "2":
+        condition = input("Condition: ")
+        delete_from_table(table_name, condition)
+    if option == "3":
+        select_table(table_name)
+    if option == "4":
+        condition = input("Condition: ")
+        search_values(table_name, condition)
+    if option == "5":
+        print_table_names()
